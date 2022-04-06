15c384480238ecbca1d6a829b6b9ae8b502e4c54
vinnidias <vinicius.3991@gmail.com>
2022-04-05 23:44:16 -0300

    root fil config to recive react navigation

--- App.js
+++ App.js
@@ -1,67 +1,16 @@
-import { SafeAreaView, TextInput, StyleSheet, View, Button } from "react-native";
-import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from "./src/Pages/Home";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  const [text, onChangeText] = React.useState("");
-  const [number, onChangeNumber] = React.useState(null);
-
-  const handleInput = (event) => {
-    onChangeText(event);
-  };
-
-  const onPress = () => {
-    console.log("text: ", text);
-  };
-
   return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => handleInput(text)}
-          value={text}
-        />
-        <Button
-          onPress={onPress}
-          title="Get"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-      </View>
-      <View style={styles.listContainer}>
-
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 20,
-    flex: 1,
-    flexDirection: "column",
-  },
-
-  headerContainer: {
-    flex: 1,
-    paddingTop: 20,
-    paddingLeft: 10,
-    alignItems: "center",
-    justifyContent: 'center',
-    flexDirection: "row",
-    backgroundColor: '#f06151',
-  },
-  
-  listContainer: {
-    flex: 7,
-
-  },
-
-  input: {
-    height: 40,
-    width: 200,
-    borderWidth: 1,
-    padding: 10,
-    marginRight: 10,
-  },
-});

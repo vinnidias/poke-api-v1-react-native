3293b3be71788ac0d64f40e079d989ae79c1b9c9
vinnidias <vinicius.3991@gmail.com>
2022-04-07 01:06:49 -0300

    create PokeCard component

--- src/components/PokeCard/index.js
+++ src/components/PokeCard/index.js
@@ -1,41 +1,65 @@
-import * as RN from 'react-native';
-import * as React from 'react';
-import axios from 'axios';
+import * as RN from "react-native";
+import * as React from "react";
+import axios from "axios";
 
-import styles from './styles'
+import styles from "./styles";
+import logo from "../../assets/pokeLogo.png";
+import { colorTypesSelector } from "../../utils/colorTypesSelector";
 
+export function PokeCard({ props, key }) {
+  const [imgPath, setImagePath] = React.useState("");
+  const [data, setData] = React.useState(undefined);
 
-export function PokeCard({props}) {
-  const [imgPath, setImagePath] = React.useState('')
-
-
-  React.useEffect(()=>{
+  React.useEffect(() => {
     const getData = async () => {
       try {
         const path = await axios.get(props.url);
         const res = path.data;
-        const dreamWorldPath =  res.sprites.front_default
-        console.log(dreamWorldPath, "antes de setar")
-        setImagePath(dreamWorldPath)
+        const dreamWorldPath = res.sprites;
+        const objectToArray = Object.entries(dreamWorldPath.other);
+        const imgPath = objectToArray[2][1].front_default;
+        setData({ ...res });
+        setImagePath(imgPath);
+      } catch (error) {}
+    };
+    getData();
+  }, [props]);
 
-      } catch (error) {
-        
-      }
-    }
-    getData()
-  },[props])
+  return (
+    <RN.View>
+      {data ? (
+        <RN.TouchableOpacity
+          style={{
+            backgroundColor: data
+              ? colorTypesSelector(data.types[0].type.name)
+              : "white",
+            ...styles.container,
+          }}
+          key={key}
+        >
+          <RN.View style={styles.info}>
+            <RN.Text style={styles.name}>{props.name.charAt(0).toUpperCase()+props.name.slice(1, props.name.lenght)}</RN.Text>
+            {
+              data && data.types.map((item,index)=> {
+                return(
+                  <RN.Text style={styles.type} key={index}>{item.type.name}</RN.Text>
+                )
+              })
+            }
+          </RN.View>
 
-  console.log("image path no estado: ", imgPath)
-  return(
-    <RN.View style={styles.container}>
-      <RN.Text style={styles.text}>
-        {props.name}
-      </RN.Text>
-
-      <RN.Image
-        source={{uri: imgPath}}
-        style={styles.tinyLogo}
-      />
+          <RN.ImageBackground
+            source={logo}
+            style={styles.logo}
+            resizeMode="cover"
+          >
+            <RN.Text style={styles.id}></RN.Text>
+            <RN.Image source={{ uri: imgPath }} style={styles.tinyLogo} />
+          </RN.ImageBackground>
+        </RN.TouchableOpacity>
+      ) : (
+        <RN.Text>Carregando</RN.Text>
+      )}
     </RN.View>
-  )
+  );
 }
--- src/components/PokeCard/styles.js
+++ src/components/PokeCard/styles.js
@@ -1,15 +1,43 @@
 import { StyleSheet } from "react-native"
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes"
 
 
 const styles = StyleSheet.create({
   container: {
     display: "flex",
-    borderWidth: 1,
-    borderColor: 'black',
-    width: 150,
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'space-between',
+    
+    width: '80%',
     height: 150,
     padding: 2,
     margin: 10,
+    padding: 10,
+    
+    borderRadius: 20
+  },
+
+  name: {
+    fontSize: 22,
+    color: 'white',
+    fontWeight: 'bold'
+  },
+
+  type: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    margin: 2,
+    borderRadius: 15,
+    padding: 1,
+    textAlign: 'center'
+  },
+
+  logo: {
+    width: 90,
+    height: 90,
   },
 
   tinyLogo: {

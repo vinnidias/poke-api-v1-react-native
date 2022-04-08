ac4418f9d5e3944fd8a7ffb6e76bfb09e2ede2c0
vinnidias <vinicius.3991@gmail.com>
2022-04-07 21:55:46 -0300

    refactoring pokeCard for navigate screen logic

--- src/components/PokeCard/index.js
+++ src/components/PokeCard/index.js
@@ -1,13 +1,15 @@
 import * as RN from "react-native";
 import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import axios from "axios";
 
 import styles from "./styles";
 import logo from "../../assets/pokeLogo.png";
+import pokeball from '../../assets/pokeBallGif.gif'
 import { colorTypesSelector } from "../../utils/colorTypesSelector";
 
-export function PokeCard({ props, key }) {
-  const [imgPath, setImagePath] = React.useState("");
+export function PokeCard({ props, onPress }) {
+  const [imgPath, setImagePath] = React.useState("./");
   const [data, setData] = React.useState(undefined);
 
   React.useEffect(() => {
@@ -20,46 +22,44 @@ export function PokeCard({ props, key }) {
         const imgPath = objectToArray[2][1].front_default;
         setData({ ...res });
         setImagePath(imgPath);
-      } catch (error) {}
+      } catch (error) {
+        console.log("req fail: ", error);
+      }
     };
     getData();
   }, [props]);
 
   return (
-    <RN.View>
-      {data ? (
-        <RN.TouchableOpacity
-          style={{
-            backgroundColor: data
-              ? colorTypesSelector(data.types[0].type.name)
-              : "white",
-            ...styles.container,
-          }}
-          key={key}
-        >
-          <RN.View style={styles.info}>
-            <RN.Text style={styles.name}>{props.name.charAt(0).toUpperCase()+props.name.slice(1, props.name.lenght)}</RN.Text>
-            {
-              data && data.types.map((item,index)=> {
-                return(
-                  <RN.Text style={styles.type} key={index}>{item.type.name}</RN.Text>
-                )
-              })
-            }
-          </RN.View>
+    <RN.TouchableOpacity
+      style={{
+        backgroundColor: data
+          ? colorTypesSelector(data.types[0].type.name)
+          : "white",
+        ...styles.container,
+      }}
+      onPress={onPress}
+    >
+      <RN.View style={styles.info}>
+        <RN.Text style={styles.name}>
+          {props.name.charAt(0).toUpperCase() +
+            props.name.slice(1, props.name.lenght)}
+        </RN.Text>
+        {data &&
+          data.types.map((item, index) => {
+            return (
+              <RN.Text style={styles.type} key={index}>
+                {item.type.name}
+              </RN.Text>
+            );
+          })}
+      </RN.View>
 
-          <RN.ImageBackground
-            source={logo}
-            style={styles.logo}
-            resizeMode="cover"
-          >
-            <RN.Text style={styles.id}>#{data && data.id}</RN.Text>
-            <RN.Image source={{ uri: imgPath }} style={styles.tinyLogo} />
-          </RN.ImageBackground>
-        </RN.TouchableOpacity>
-      ) : (
-        <RN.Text>Carregando</RN.Text>
-      )}
-    </RN.View>
+      <RN.ImageBackground source={logo} style={styles.logo} resizeMode="cover">
+        <RN.Text style={styles.id}>#{data && data.id}</RN.Text>
+       {data ? <RN.Image source={{ uri: imgPath }} style={styles.tinyLogo} />
+        : <RN.Image source={pokeball} style={styles.tinyLogo}/> 
+      }
+      </RN.ImageBackground>
+    </RN.TouchableOpacity>
   );
 }

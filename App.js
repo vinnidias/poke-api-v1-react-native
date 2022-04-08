ece46465fdf7a054e58caec4ba8acc859db32887
vinnidias <vinicius.3991@gmail.com>
2022-04-07 21:53:36 -0300

    adding screen details

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import { Home } from "./src/Pages/Home";
 import { Pokedex } from "./src/Pages/Pokedex";
+import { Details } from "./src/Pages/Details";
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Pokédex" component={Pokedex} />
+        <Stack.Screen name="Details" component={Details}/>
       </Stack.Navigator>
     </NavigationContainer>
   );

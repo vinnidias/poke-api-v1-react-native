be10881b72acfe58c93b349c850ddeb7f9083a99
vinnidias <vinicius.3991@gmail.com>
2022-04-13 01:41:04 -0300

    configuring context provider for app

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import { PokeDataProvider } from "./src/contexts/PokeDataContexts";
 import { Home } from "./src/Pages/Home";
 import { Pokedex } from "./src/Pages/Pokedex";
 import { Details } from "./src/Pages/Details";
@@ -10,11 +11,13 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Pokédex" component={Pokedex} />
-        <Stack.Screen name="Details" component={Details}/>
-      </Stack.Navigator>
+      <PokeDataProvider>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Pokédex" component={Pokedex} />
+          <Stack.Screen name="Details" component={Details} />
+        </Stack.Navigator>
+      </PokeDataProvider>
     </NavigationContainer>
   );
 }

1e6179e7fdd89de0279dfecfa5b3015efaf43d82
vinnidias <vinicius.3991@gmail.com>
2022-04-12 21:56:39 -0300

    same correcting

--- src/utils/changeFirstStringIndexToUpperCase.js
+++ src/utils/changeFirstStringIndexToUpperCase.js
@@ -2,4 +2,4 @@ const changeFirstStringIndexToUpperCase = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1, string.lenght);
 };
 
-export { changeFirstStringToUperCase };
+export { changeFirstStringIndexToUpperCase };

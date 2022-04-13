63f34d7fa7f135aca61b1bab3ee87f8a79b67699
vinnidias <vinicius.3991@gmail.com>
2022-04-13 01:50:29 -0300

    refactoring handle press with context function

--- src/Pages/Pokedex/index.js
+++ src/Pages/Pokedex/index.js
@@ -2,14 +2,17 @@ import * as RN from "react-native";
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
+import styles from "./styles";
+import pokeball from "../../assets/pokeBallGif.gif";
 import { pokeApiClient } from "../../services/pokeApiClient";
 import { PokeCard } from "../../components/PokeCard";
-import styles from "./styles";
-import pokeball from '../../assets/pokeBallGif.gif'
+import { PokeDataContexts } from "../../contexts/PokeDataContexts";
 
 export function Pokedex({ navigation }) {
   const [list, setList] = React.useState([]);
   const [offset, setOffset] = React.useState(0);
+  const { setData } = React.useContext(PokeDataContexts);
+
   React.useMemo(() => {
     (async () => {
       try {
@@ -17,26 +20,45 @@ export function Pokedex({ navigation }) {
           `pokemon?offset=${offset}&limit=20`
         );
         const res = path.data.results;
-        setList(prev => [...prev, ...res]);
+        setList((prev) => [...prev, ...res]);
       } catch (error) {
         console.log("req fail: ", { ...error });
       }
     })();
-    return ()=> setList([])
+    return () => setList([]);
   }, [offset]);
 
   return (
     <RN.SafeAreaView style={styles.container}>
-      {list.length !== 0 ? <RN.ScrollView style={styles.srollContainer}>
-        {list.map((item, index) => {
-          return <PokeCard props={item} key={index} onPress={()=> navigation.navigate('Details', {...item})}/>;
-        })}
-        <RN.TouchableOpacity>
-          <RN.Text onPress={()=> setOffset(offset + 20)} style={styles.loadMore}>Load more...</RN.Text>
-        </RN.TouchableOpacity>
-      </RN.ScrollView>
-      : <RN.Image source={pokeball} style={{alignSelf: "center", marginTop: '50%'}}/>  
-    }
+      {list.length !== 0 ? (
+        <RN.ScrollView style={styles.srollContainer}>
+          {list.map((item, index) => {
+            return (
+              <PokeCard
+                props={item}
+                key={index}
+                onPress={(data) => {
+                  setData(data);
+                  navigation.navigate("Details");
+                }}
+              />
+            );
+          })}
+          <RN.TouchableOpacity>
+            <RN.Text
+              onPress={() => setOffset(offset + 20)}
+              style={styles.loadMore}
+            >
+              Load more...
+            </RN.Text>
+          </RN.TouchableOpacity>
+        </RN.ScrollView>
+      ) : (
+        <RN.Image
+          source={pokeball}
+          style={{ alignSelf: "center", marginTop: "50%" }}
+        />
+      )}
     </RN.SafeAreaView>
   );
 }

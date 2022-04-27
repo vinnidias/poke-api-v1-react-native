23c5b9eb0ba589b1651859ea752b9eda7fac603f
vinnidias <vinicius.3991@gmail.com>
2022-04-27 02:21:24 -0300

    removing comented script

--- src/Pages/Pokedex/styles.js
+++ src/Pages/Pokedex/styles.js
@@ -21,33 +21,8 @@ export const Label = styled.Text`
 export const LoaderIcon = styled.Image`
   align-self: center;
   margin-top: 10px;
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  bottom: 0;
 `
-
-// import { StyleSheet } from "react-native";
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-
-//   logo: {
-//     flex: 1,
-//     width: '100%',
-//     paddingVertical: 60,
-//     alignItems: 'center',
-//   },
-
-//   srollContainer: {
-//     flex: 2,
-//     maxHeight: '80%',
-//     width: "100%",
-//     marginTop: 50,
-//   },
-
-//   loadMore: {
-//     alignSelf: 'center',
-//     fontSize: 18,
-//   }
-// });
-
-// export default styles

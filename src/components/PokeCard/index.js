629492464631550864c15dd257cc71febbc781bc
vinnidias <vinicius.3991@gmail.com>
2022-04-28 03:33:27 -0300

    refactoring pokeCard with styled components

--- src/components/PokeCard/index.js
+++ src/components/PokeCard/index.js
@@ -1,19 +1,25 @@
 import { useMemo, useState } from "react";
+import { Text } from 'react-native'
 import { NavigationContainer } from "@react-navigation/native";
 import axios from "axios";
 
 import { changeFirstStringIndexToUpperCase } from "../../utils/changeFirstStringIndexToUpperCase";
 import logo from "../../assets/pokeLogo.png";
 import pokeball from "../../assets/pokeBallGif.gif";
+import hpEmoji from "../../assets/ez_heart.gif";
 import {
   TouchableContainer,
   DataContainer,
   Name,
   Type,
   PokeBallBackground,
+  TopTextContainer,
   PokeId,
+  Hp,
+  Attack,
   PokeSprite,
   LoaderIcon,
+  Defense
 } from "./styles.js";
 
 export function PokeCard({ props, onPress }) {
@@ -41,34 +47,51 @@ export function PokeCard({ props, onPress }) {
   }, [props]);
 
   return (
-    <TouchableContainer
-      style={{ elevation: 15, shadowColor: "#000" }}
-      onPress={() => onPress(data)}
-      typeBackgroundColor={data ? data.types[0].type.name : ""}
-    >
-      <DataContainer>
-        <Name>{changeFirstStringIndexToUpperCase(props.name)}</Name>
-        {data &&
-          data.types.map((item, index) => {
-            return (
-              <Type
-                key={index}
-                typeColor={item.type.name}
-                style={{ elevation: 3, shadowColor: "#000" }}
-              >
-                {item.type.name}
-              </Type>
-            );
-          })}
-      </DataContainer>
-      <PokeBallBackground source={logo} resizeMode="cover">
-        <PokeId>#{data && data.id}</PokeId>
-        {data ? (
-          <PokeSprite source={{ uri: imgPath }} />
-        ) : (
-          <LoaderIcon source={pokeball} />
-        )}
-      </PokeBallBackground>
-    </TouchableContainer>
+    <>
+      {isLoading ? (
+        <LoaderIcon source={pokeball} />
+      ) : (
+        <TouchableContainer
+          style={{ elevation: 30, shadowColor: "#000" }}
+          onPress={() => onPress(data)}
+          typeBackgroundColor={data ? data.types[0].type.name : ""}
+        >
+          <DataContainer>
+            <Name>{changeFirstStringIndexToUpperCase(props.name)}</Name>
+            {data &&
+              data.types.map((item, index) => {
+                return (
+                  <Type
+                    key={index}
+                    typeColor={item.type.name}
+                    style={{ elevation: 3, shadowColor: "#000" }}
+                  >
+                    {item.type.name}
+                  </Type>
+                );
+              })}
+          </DataContainer>
+          <PokeBallBackground source={logo} resizeMode="cover">
+            <TopTextContainer>
+              <Hp>
+                {data && data.stats[0].base_stat}{" "}
+                {data && changeFirstStringIndexToUpperCase(data.stats[0].stat.name)}
+              </Hp>
+              <PokeId>#{data && data.id}</PokeId>
+            </TopTextContainer>
+
+            <PokeSprite source={{ uri: imgPath }} />
+          </PokeBallBackground>
+          <Attack>
+            {data && changeFirstStringIndexToUpperCase(data.stats[1].stat.name)}:{" "}
+            {data && data.stats[1].base_stat}
+          </Attack>
+          <Defense>
+            {data && changeFirstStringIndexToUpperCase(data.stats[2].stat.name)}:{" "}
+            {data && data.stats[2].base_stat}
+          </Defense>
+        </TouchableContainer>
+      )}
+    </>
   );
 }
--- src/components/PokeCard/styles.js
+++ src/components/PokeCard/styles.js
@@ -1,8 +1,9 @@
-import styled  from "styled-components/native"
+import styled from "styled-components/native";
 import { colorTypesSelector } from "../../utils/colorTypesSelector";
 
 export const TouchableContainer = styled.TouchableOpacity`
-  background-color: ${props => colorTypesSelector(props.typeBackgroundColor).replace(/"/g, "")};
+  background-color: ${(props) =>
+    colorTypesSelector(props.typeBackgroundColor).replace(/"/g, "")};
   display: flex;
   flex-direction: row;
   align-self: center;
@@ -29,7 +30,7 @@ export const Type = styled.Text`
   font-size: 16px;
   color: white;
   font-weight: bold;
-  background-color: ${props => colorTypesSelector(props.typeColor)};
+  background-color: ${(props) => colorTypesSelector(props.typeColor)};
   margin: 3px;
   border-radius: 15px;
   padding: 4px;
@@ -41,8 +42,22 @@ export const PokeBallBackground = styled.ImageBackground`
   height: 90px;
 `;
 
+export const TopTextContainer = styled.View`
+  flex-direction: row;
+`;
+
 export const PokeId = styled.Text`
-  opacity: .5;
+  opacity: 0.8;
+  color: white;
+  position: absolute;
+  right: 0;
+`;
+
+export const Hp = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: white;
+  margin-right: 10px;
 `;
 
 export const PokeSprite = styled.Image`
@@ -53,4 +68,25 @@ export const PokeSprite = styled.Image`
 export const LoaderIcon = styled.Image`
   width: 90px;
   height: 90px;
+  margin: 0 auto;
+`;
+
+export const Attack = styled.Text`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  margin: 0px 0px 5px 15px;
+  color: white;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+export const Defense = styled.Text`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  margin: 0px 15px 5px 0px;
+  color: white;
+  font-size: 14px;
+  font-weight: bold;
 `;

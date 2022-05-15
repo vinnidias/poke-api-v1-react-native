22e21270820cb89a9ff9b158b013dfc9c4166277
vinnidias <vinicius.3991@gmail.com>
2022-05-15 18:43:20 -0300

    adding new routes to root App

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import { PokeDataProvider } from "./src/contexts/PokeDataContexts";
+import { TrainerProvider } from "./src/contexts/TrainerContexts";
 import { Home } from "./src/Pages/Home";
 import { Pokedex } from "./src/Pages/Pokedex";
 import { Details } from "./src/Pages/Details";
+import { Login } from "./src/Pages/Login";
+import { Register } from "./src/Pages/Register";
+
 
 const Stack = createStackNavigator();
 
@@ -12,11 +16,29 @@ export default function App() {
   return (
     <NavigationContainer>
       <PokeDataProvider>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Pokédex" component={Pokedex} />
-          <Stack.Screen name="Details" component={Details} />
-        </Stack.Navigator>
+        <TrainerProvider>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Register"
+              component={Register}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+                headerLeft: false,
+              }}
+            />
+            <Stack.Screen name="Pokédex" component={Pokedex} />
+            <Stack.Screen name="Details" component={Details} />
+          </Stack.Navigator>
+        </TrainerProvider>
       </PokeDataProvider>
     </NavigationContainer>
   );
